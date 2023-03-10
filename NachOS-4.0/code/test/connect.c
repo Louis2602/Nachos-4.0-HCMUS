c73616862bbfaf656d8c985445b7ebb971ecdab2
Louis2602 <trantunglam135790@gmail.com>
2023-03-10 23:54:30 +0700

    update

--- NachOS-4.0/code/test/connect.c
+++ NachOS-4.0/code/test/connect.c
@@ -4,7 +4,7 @@ int main()
 {
     int status;
     char *ip = "127.0.0.1";
-    int port = 80;
+    int port = 9001;
     int sockID = 6;
     status = Connect(sockID, ip, port);
     Halt();
--- NachOS-4.0/code/test/server.c
+++ NachOS-4.0/code/test/server.c
@@ -1,39 +1,81 @@
-#include <netinet/in.h> //structure for storing address information
+// #include <netinet/in.h> //structure for storing address information
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/socket.h> //for socket APIs
+// #include <sys/types.h>
+// #include <arpa/inet.h>
+// #include <sys/un.h>
+// #include <string.h>
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h> //for socket APIs
+#include <string.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SERVER_PATH "/tmp/server"
 
 int main(int argc, char const *argv[])
 {
 
-    // create server socket similar to what was done in
-    // client program
-    int servSockD = socket(AF_INET, SOCK_STREAM, 0);
-
+    int rc;
+    // define server address
+    struct sockaddr_un servAddr;
+    int servSockD = -1, clientSocket = -1;
     // string store data to send to client
     char serMsg[255] = "Message from the server to the "
                        "client \'Hello Client\' ";
+    do
+    {
+        servSockD = socket(AF_UNIX, SOCK_DGRAM, 0);
+        if (servSockD < 0)
+        {
+            printf("Error: Socket failed.\n");
+            return -1;
+        }
 
-    // define server address
-    struct sockaddr_in servAddr;
+        // struct sockaddr_in servAddr;
 
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(9001);
-    servAddr.sin_addr.s_addr = INADDR_ANY;
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sun_family = AF_UNIX;
+        strcpy(servAddr.sun_path, SERVER_PATH);
 
-    // bind socket to the specified IP and port
-    bind(servSockD, (struct sockaddr *)&servAddr,
-         sizeof(servAddr));
+        // inet_pton(AF_INET, ip, &servAddr.sin_addr); // Convert IP address to binary form
 
-    // listen for connections
-    listen(servSockD, 1);
+        // bind socket to the specified IP and port
+        // bind(servSockD, (struct sockaddr *)&servAddr, sizeof(servAddr));
+        rc = bind(servSockD, (struct sockaddr *)&servAddr, sizeof(sa_family_t));
+        // rc = bind(servSockD, (struct sockaddr *)&servAddr, SUN_LEN(&servAddr));
+        if (rc < 0)
+        {
+            printf("Error: Bind failed.\n");
+            return -1;
+        }
+        // listen for connections
+        rc = listen(servSockD, 20);
+        if (rc < 0)
+        {
+            printf("Error: Listen failed.\n");
+            return -1;
+        }
 
-    // integer to hold client socket.
-    int clientSocket = accept(servSockD, NULL, NULL);
+        printf("Ready for client connect.\n");
+        // integer to hold client socket.
+        clientSocket = accept(servSockD, NULL, NULL);
+        if (clientSocket < 0)
+        {
+            printf("Error: Accept failed.\n");
+            return -1;
+        }
+        // send's messages to client socket
+        rc = send(clientSocket, serMsg, sizeof(serMsg), 0);
+        if (rc < 0)
+        {
+            printf("Error: Send failed.\n");
+            return -1;
+        }
 
-    // send's messages to client socket
-    send(clientSocket, serMsg, sizeof(serMsg), 0);
+    } while (0);
 
     return 0;
 }
