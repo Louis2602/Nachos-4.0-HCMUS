4bf6fb73a59ae478fd9da3c49ab701cc721ffdac
Louis2602 <trantunglam135790@gmail.com>
2023-03-20 23:57:53 +0700

    update

--- NachOS-4.0/code/test/client.c
+++ NachOS-4.0/code/test/client.c
@@ -9,30 +9,34 @@ int main()
     int port = 9000;
     int dataSize = 0;
 
-    // char *data = "Hello World";
-    char *data;
+    char *data = "Hello World";
+    // char *data;
     char buffer[BUFFER_LENGTH];
     int len = sizeof(buffer);
+    int i;
 
-    // Open a socket
-    socketid = SocketTCP();
-    // Connect to a server
-    status = Connect(socketid, ip, port);
-
-    while (1)
+    for (i = 0; i < 20; i++)
     {
-        PrintString("Enter message: ");
-        ReadString(data, BUFFER_LENGTH);
+        // Open a socket
+        socketid = SocketTCP();
+        // Connect to a server
+        status = Connect(socketid, ip, port);
+
+        // while (1)
+        // {
+        // PrintString("Enter message: ");
+        // ReadString(data, BUFFER_LENGTH);
         while (data[dataSize] != '\0')
             dataSize++;
         // Send data to the server
         noBytes = Send(socketid, data, dataSize);
         // Receive buffer echoed back from server
         noBytes = Receive(socketid, data, len);
+        // }
+        // Close socket
+        if (socketid == -1)
+            status = CloseSocketTCP(socketid);
     }
-    // Close socket
-    if (socketid == -1)
-        status = CloseSocketTCP(socketid);
 
     Halt();
 }
